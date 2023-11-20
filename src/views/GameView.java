6dd560e88f59eb26ed15de3e3dfe2bfa59c1f9ca
dorukwagle <dorukwagle@gmail.com>
2023-11-20 18:49:27 +0545

    create game view

--- src/views/GameView.java
+++ src/views/GameView.java
@@ -1,48 +1,67 @@
 package views;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.Arrays;
 
 public class GameView extends JPanel {
 
     private ImageIcon puzzle;
     private JButton[] numPad = new JButton[10];
-    private JLabel gameState, lifeLine;
+    private JLabel gameState, lifeLine, gameScore;
+    private JButton logOutBtn, resetBtn;
+
+    private String defaultStateText = "Guess Hidden Number!";
 
     public GameView() {
-        setBorder(new EmptyBorder(20, 20, 20, 20));
-        setLayout(new FlowLayout());
+        setBorder(new EmptyBorder(20, 10, 20, 10));
+        setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.anchor = GridBagConstraints.WEST;
 
         var gamePanel = new JPanel();
-        gamePanel.setLayout(new BoxLayout(gamePanel, BoxLayout.Y_AXIS));
+        gamePanel.setLayout(new BoxLayout(gamePanel, BoxLayout.PAGE_AXIS));
         populateGamePanel(gamePanel);
-        add(gamePanel);
+        constraints.weightx = 0.5;
+        constraints.gridx = 0;
+
+        add(gamePanel, constraints);
 
         var infoPanel = new JPanel();
-        infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.Y_AXIS));
+        infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.PAGE_AXIS));
         populateInfoPanel(infoPanel);
-        add(infoPanel);
+
+        constraints.gridx = 1;
+        constraints.anchor = GridBagConstraints.NORTHWEST;
+        add(infoPanel, constraints);
     }
 
     private void populateGamePanel(JPanel parent) {
-        puzzle = new ImageIcon();
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(getClass().getClassLoader().getResource("res/gameover.png"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        puzzle = new ImageIcon(img.getScaledInstance(MainWindow.widthFactor(0.7), MainWindow.heightFactor(0.7), Image.SCALE_SMOOTH));
         var puzzleContainer = new JLabel(puzzle);
-        puzzleContainer.setSize(new Dimension(MainWindow.widthFactor(0.5), MainWindow.heightFactor(0.7)));
+        puzzleContainer.setAlignmentX(Component.CENTER_ALIGNMENT);
+        puzzleContainer.setBackground(Color.blue);
+
         parent.add(puzzleContainer);
 
-        var statusPanel = new JPanel();
-        statusPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 20, 0));
+        var stateHolder = new JPanel();
+        stateHolder.setLayout(new FlowLayout(FlowLayout.LEFT));
+        gameState = new JLabel(defaultStateText);
+        gameState.setFont(new Font("Serif", Font.BOLD, 20));
 
-        lifeLine = new JLabel();
-        lifeLine.setFont(new Font("Serif", Font.BOLD, 15));
-        statusPanel.add(lifeLine);
-
-        gameState = new JLabel();
-        gameState.setFont(new Font("Serif", Font.BOLD, 15));
-        statusPanel.add(gameState);
-        parent.add(statusPanel);
+        stateHolder.add(gameState);
+        parent.add(createSpace());
+        parent.add(stateHolder);
 
         // add solution buttons
         var solutionPad = new JPanel();
@@ -54,11 +73,45 @@ public class GameView extends JPanel {
             solutionPad.add(btn);
         }
 
+        parent.add(createSpace());
         parent.add(solutionPad);
     }
 
     private void populateInfoPanel(JPanel parent) {
+        var playerName = new JLabel("Player: Test Player");
+        playerName.setFont(new Font("Serif", Font.BOLD, 25));
+        parent.add(playerName);
 
+        parent.add(createSpace());
+
+        gameScore = new JLabel("Score: 0");
+        gameScore.setFont(new Font("Serif", Font.BOLD, 20));
+        parent.add(gameScore);
+
+        lifeLine = new JLabel("Lives: ");
+        lifeLine.setFont(new Font("Serif", Font.BOLD, 20));
+        parent.add(lifeLine);
+
+        parent.add(createSpace());
+
+        var controlPanel = new JPanel();
+        controlPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 0));
+
+        resetBtn = new JButton("Reset Game");
+        resetBtn.setFont(new Font("Serif", Font.BOLD, 20));
+        parent.add(resetBtn);
+
+        parent.add(createSpace());
+
+        logOutBtn = new JButton("Log Out");
+        logOutBtn.setFont(new Font("Serif", Font.BOLD, 20));
+        parent.add(logOutBtn);
+
+        parent.add(controlPanel);
+    }
+
+    private Component createSpace() {
+        return Box.createRigidArea(new Dimension(5, 20));
     }
 
     public void setLifeLine(int lifeLines) {
@@ -81,12 +134,34 @@ public class GameView extends JPanel {
     }
 
     public void alertGameOver() {
-        // change the puzzle and disable all the buttons
+        // disable all the buttons
+        Arrays.stream(numPad).forEach(btn -> btn.setEnabled(false));
+    }
 
+    public void resetGame() {
+        // enable all the buttons
+        Arrays.stream(numPad).forEach(btn -> btn.setEnabled(true));
+        this.gameState.setText(defaultStateText);
     }
 
     public void setPuzzle(BufferedImage image) {
         this.puzzle.setImage(image);
         this.repaint();
+    }
+
+    public void setGameScore(int score) {
+        this.gameScore.setText(String.valueOf(score));
+    }
+
+    public JButton[] getNumPad() {
+        return numPad;
+    }
+
+    public JButton getLogOutBtn() {
+        return logOutBtn;
+    }
+
+    public JButton getResetBtn() {
+        return resetBtn;
     }
 }
